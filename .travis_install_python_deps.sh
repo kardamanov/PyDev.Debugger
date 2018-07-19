5a0c31e6ab23c06961fa96676b6396b2ab1fa081
fabioz <fabiofz@gmail.com>
2018-07-19 13:34:38 -0300

    fixup

--- .travis_install_python_deps.sh
+++ .travis_install_python_deps.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -ev
 
-conda install --yes numpy ipython cython pytest pytest-xdist psutil
+conda install --yes numpy ipython cython pytest psutil
 
-if [ "$TRAVIS_PYTHON_VERSION" = "2.6" ]; then
+if [ "$PYDEVD_PYTHON_VERSION" = "2.6" ]; then
     conda install --yes pyqt=4
     # Django 1.7 does not support Python 2.7
 fi
-if [ "$TRAVIS_PYTHON_VERSION" = "2.7" ]; then
+if [ "$PYDEVD_PYTHON_VERSION" = "2.7" ]; then
     conda install --yes pyqt=4
     pip install "django>=1.7,<1.8"
 
 fi
-if [ "$TRAVIS_PYTHON_VERSION" = "3.5" ]; then
+if [ "$PYDEVD_PYTHON_VERSION" = "3.5" ]; then
     conda install --yes pyqt=5
     pip install "django>=1.7,<1.8"
 fi
 
 pip install Pympler
+# pytest-xdist not available on conda for python 2.6
+pip install pytest-xdist
 pip install untangle
 pip install scapy==2.4.0
